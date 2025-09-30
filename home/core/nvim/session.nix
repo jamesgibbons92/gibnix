e98d334d4634b3cc79fd8e99db936db26405fa04
James <jay.m.gibbons@gmail.com>
2025-09-30 21:40:09 +0100

    meg

--- home/core/nvim/session.nix
+++ home/core/nvim/session.nix
@@ -9,9 +9,7 @@
       lua
       */
       ''
-        require("auto-session").setup({
-          auto_restore_last_session = true,
-        })
+        require("auto-session").setup({})
       '';
   };
 }
