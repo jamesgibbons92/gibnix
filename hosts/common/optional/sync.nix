3fea9b94042c11b710f9845dcf5cc5db351ecf83
James <10496761+jamesgibbons92@users.noreply.github.com>
2026-01-07 00:25:22 +0000

    lol

--- hosts/common/optional/sync.nix
+++ hosts/common/optional/sync.nix
@@ -1,4 +1,33 @@
 {config, ...}: {
+  networking.hosts = {
+    "127.0.0.1" = ["syncthing"];
+  };
+
+  services.nginx = {
+    enable = true;
+    virtualHosts."syncthing" = {
+      listen = [
+        {
+          addr = "127.0.0.1";
+          port = 80;
+        }
+      ];
+      serverName = "syncthing";
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:8384";
+        proxyWebsockets = true;
+        extraConfig = ''
+          proxy_set_header Host $host;
+          proxy_set_header X-Real-IP $remote_addr;
+          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+          proxy_set_header X-Forwarded-Proto $scheme;
+          proxy_read_timeout 600s;
+          proxy_send_timeout 600s;
+        '';
+      };
+    };
+  };
+
   services.syncthing = {
     enable = true;
     user = "james";
@@ -8,7 +37,11 @@
     # Homelab is an introducer which we can then use to add the other nodes.
     overrideDevices = false;
     overrideFolders = false;
+    guiAddress = "127.0.0.1:8384";
     settings = {
+      gui = {
+        insecureSkipHostCheck = true;
+      };
       devices = {
         "homelab" = {
           id = "IP2WGJC-CY7LDYV-QAM24HD-5BWYYCO-ZH6OCEW-UUAQWNY-AKFYFGO-5GFXHQX";
@@ -39,6 +72,7 @@
       options = {
         urAccepted = -1;
         startBrowser = true;
+
         # Disable all discovery
         natEnabled = false;
         relaysEnabled = false;
