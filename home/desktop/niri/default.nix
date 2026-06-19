238a97fa33887c4c3917572a9dea0050fbcba6ce
James <10496761+jamesgibbons92@users.noreply.github.com>
2026-06-19 22:19:46 +0100

    monitors

--- home/desktop/niri/default.nix
+++ home/desktop/niri/default.nix
@@ -80,9 +80,6 @@
             position.y = 0;
           };
           "HDMI-A-1" = {
-            # transform.rotation = 90;
-            # position.x = -1080;
-            # position.y = 0;
             mode = {
               width = 2560;
               height = 1440;
--- home/hosts/wukong.nix
+++ home/hosts/wukong.nix
@@ -7,4 +7,12 @@
 
   desktop.windowManager = "niri";
   desktop.keyboardLayout = "gb";
+  programs.niri.settings.outputs = {
+    "HDMI-A-1" = {
+      transform.rotation = 90;
+      position.x = -1080;
+      position.y = 0;
+      # position.y = 0;
+    };
+  };
 }
